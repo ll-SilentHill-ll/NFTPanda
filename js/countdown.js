e0597901e551cd1dab48e94c1e59b00ca4b17268
ll-SilentHill-ll <krasuny4@ukr.net>
2023-08-13 22:30:25 +0300

    Update countdown.js

---
+++ js/countdown.js
@@ -0,0 +1,29 @@
+function countdown(element, targetTime) {
+  function updateCountdown() {
+    const currentTime = new Date().getTime();
+    const timeDifference = targetTime - currentTime;
+
+    if (timeDifference <= 0) {
+      element.innerHTML = "Время истекло";
+      return;
+    }
+
+    const minutesLeft = Math.floor(timeDifference / (60 * 1000));
+    const secondsLeft = Math.floor((timeDifference % (60 * 1000)) / 1000);
+
+    element.innerHTML = `Осталось: ${minutesLeft} мин ${secondsLeft} сек`;
+  }
+
+  updateCountdown();
+  const countdownInterval = setInterval(updateCountdown, 1000);
+}
+
+// Получаем все элементы с классом sale_timer
+const timerElements = document.querySelectorAll('.sale_timer');
+
+// Устанавливаем обратный отсчет для каждого элемента
+timerElements.forEach(element => {
+  const targetTimeString = element.getAttribute('data-target-time');
+  const targetTime = new Date(targetTimeString).getTime();
+  countdown(element, targetTime);
+});
